bc006925d4c6b23d62ce92fef929680ca78a2341
Aurthur Musendame <aurthurmusendame@gmail.com>
2022-12-01 13:32:24 +0200

    refactored docker to map port 8080 t0 8082

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM openjdk:11
 EXPOSE 80
+EXPOSE 8080
 ADD ./target/poc-device-integration-0.0.1-SNAPSHOT.jar poc-device-integration-0.0.1-SNAPSHOT.jar
 ENTRYPOINT ["java","-jar", "/poc-device-integration-0.0.1-SNAPSHOT.jar"]
--- poc_device.sh
+++ poc_device.sh
@@ -1,5 +1,5 @@
 echo "Building poc device integration app for production ....";
-mvn clean install -DskipTests -Dspring.profiles.active=prod
+mvn clean install -DskipTests -Dspring.profiles.active=dev
 docker-compose build pocdeviceintegration
 docker-compose up -d pocdeviceintegration
 echo "Done buiding poc device integration:)";
